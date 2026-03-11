a210f390eb461c772c1cd5da8f211784a196ce6b
Muhammad-Asim-raza <asimraza9659@gmail.com>
2026-03-11 10:34:04 -0700

    Build Text To Voice Converter Using JavaScript

---
+++ 18-speechGenerator/script.js
@@ -0,0 +1,16 @@
+let speech= new SpeechSynthesisUtterance();
+let btn= document.getElementById("btn");
+let voices=[];
+let voiceSelect = document.querySelector("select");
+window.speechSynthesis.onvoiceschanged = ()=>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice =voices[0];
+    voices.forEach((voice,i)=>(voiceSelect.options[i]= new Option(voice.name, i)));
+}
+voiceSelect.addEventListener("change", ()=>{
+    speech.voice = voices[voiceSelect.value];
+})
+btn.addEventListener("click",()=>{
+    speech.text= document.getElementById("text").value;
+    window.speechSynthesis.speak(speech);
+})
